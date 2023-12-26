036be2d1e13429d9d2731de517bae0cffe7d22d8
petzl300lumen <134718172+petzl300lumen@users.noreply.github.com>
2023-12-26 10:52:25 +0300

    Create fill.sql

---
+++ fill.sql
@@ -0,0 +1,8 @@
+INSERT INTO aplicationss (id,date_add,type_cur,description,status,id_animal,id_client) VALUES (1,"26.12.2023","type cur description","description1","status1",1,1);
+INSERT INTO aplicationss (id,date_add,type_cur,description,status,id_animal,id_client) VALUES (2,"25.12.2023","type cur description2","description2","status2",2,2);
+
+INSERT INTO animals (id_animal, description) VALUES (1,"dercr1");
+INSERT INTO animals (id_animal, description) VALUES (2,"dercr2");
+
+INSERT INTO animals (id_client, FIO,id_animal) VALUES (1,"Иванов Иван Иванович",1);
+INSERT INTO animals (id_client, FIO,id_animal) VALUES (2,"Петр Петров Петрович",2);
